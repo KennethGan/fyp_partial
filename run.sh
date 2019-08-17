988da6bfa456a037a2c13e1074adf393867b11fb
Kenneth Gan <54207033+KennethGan@users.noreply.github.com>
2019-08-17 21:13:48 +0800

    Add files via upload

---
+++ run.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#!/opt/movidius/caffe/python
+x-terminal-emulator -e python3 -i ~/Desktop/fyp_partial-master/main.py  -m models/person-vehicle-bike-detection-crossroad-0078.xml -i sample.mp4 -d MYRIAD -k 7 -o Output.avi
+
+
+
+
+			
